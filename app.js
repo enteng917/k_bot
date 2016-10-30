4d37a601081c3005b441f19ac44579b87780286b
unknown <ij00700276@adgroup.local>
2016-10-30 10:59:16 +0900

    1st commit

---
+++ app.js
@@ -0,0 +1,30 @@
+var restify = require('restify');
+var builder = require('botbuilder');
+
+//=========================================================
+// Bot Setup
+//=========================================================
+
+// Setup Restify Server
+var server = restify.createServer();
+var botenv = 'MYPC';
+server.listen(process.env.port || process.env.PORT || 3978, function () {
+   console.log('%s listening to %s (%s)', server.name, server.url, botenv);
+});
+
+// Create chat bot
+var connector = new builder.ChatConnector({
+    appId: '8673857a-3ccd-470a-a079-408e0cdc317e',
+    appPassword: '22iKbgbmZa5CaOgxxdSF5ze'
+});
+var bot = new builder.UniversalBot(connector);
+server.post('/api/messages', connector.listen());
+
+//=========================================================
+// Bots Dialogs
+//=========================================================
+
+bot.dialog('/', function (session) {
+
+    session.send("Hello World from " + botenv );
+});

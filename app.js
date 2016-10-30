3fc7fc267d88f3ff0735fb7d02cb291199310e53
unknown <ij00700276@adgroup.local>
2016-10-30 17:45:03 +0900

    commit

--- app.js
+++ app.js
@@ -19,7 +19,8 @@ var connector = new builder.ChatConnector({
 var bot = new builder.UniversalBot(connector);
 server.post('/api/messages', connector.listen());
 
-var luis = 'https://api.projectoxford.ai/luis/v1/application?id=23da40c1-2009-4a4b-8298-474a0f929ed6&subscription-key=41d38fbedaa14628869b6883d45de360';
+//var luis = 'https://api.projectoxford.ai/luis/v1/application?id=23da40c1-2009-4a4b-8298-474a0f929ed6&subscription-key=41d38fbedaa14628869b6883d45de360';
+var luis = 'https://api.projectoxford.ai/luis/v1/application?id=23da40c1-2009-4a4b-8298-474a0f929ed6&subscription-key=41d38fbedaa14628869b6883d45de360&u=';
 var recognizer = new builder.LuisRecognizer(luis);
 var intents = new builder.IntentDialog({ recognizers: [recognizer] });
 
@@ -29,12 +30,12 @@ var intents = new builder.IntentDialog({ recognizers: [recognizer] });
 // Bots Dialogs
 //=========================================================
 
-//bot.dialog('/', function (session) {
-//    session.send(session.message);
-//});
-bot.dialog('/', intents);
+bot.dialog('/', function (session) {
+    session.send(session.message);
+    intents.replyReceived(session);
+});
+//bot.dialog('/', intents);
 intents.matches(/^greeting/i, function(session, args) {
-    console.log(args);
     session.send('greeting');
     session.send(args);
 });
